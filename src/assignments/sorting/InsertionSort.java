5690b9ee1ec38c20a30b2a9c1323333d367ad5bc
PavelKapkaev <pkapkaev@hcc-nd.edu>
2026-09-05 21:07:12 -0400

    InsertionSort

--- src/assignments/sorting/InsertionSort.java
+++ src/assignments/sorting/InsertionSort.java
@@ -1,25 +1,26 @@
 package assignments.sorting;
 
 /**
- * Swap all elements one by one until the whole array is sorted.
+ * Insert each element one by one until the array is sorted.
  */
 public class InsertionSort extends SortingAlgorithm {
   /**
-   * sort an arrray in-place using insetion sort.
+   * Sort an array in place using insetion sort.
    * 
-   * Post conditon: 'array' is sorted in ascentding order.
+   * Postcondition: 'array' is sorted in ascending order.
    * 
    * @param array an array of integers
    */
 
   public void sort(Integer[] array) {
-    for (int k = array.length;k >= 2  ;k --) {       // k is the length of thesub-array we are
-      for (int i = 0; i < k-1; i ++) {    //go up yo k - 1 because we dont need to campare the last index to its' neighbour
-        if (array[i] > array[i-1]) {
-          // swap asjacent itemsI (i and i - 1)
-          swap(array, i, i - 1);
+    for (int k = 1;k < array.length;k ++) {       // k is the index of elements we are sorting.
+      for (int i = k; i > 0; i --) {    // Compare the current element with the element before it.
+        if (array[i-1] > array[i]) {
+          // Swap adjacent items (i-1 and i).
+          swap(array, i-1, i);
         }
         else {
+          // Stop when elements are in the correct place.
           break;
 
         }
@@ -28,12 +29,10 @@ public class InsertionSort extends SortingAlgorithm {
     }
   }
   /**
-   * 
-   * swap two elements within array
+   * Swap two elements within array.
    * @param array the array to swap value in
    * @param i
    * @param j
-   * 
   */
   private void swap(Integer[] array, int i, int j) {
     Integer temp = array[i];
@@ -43,30 +42,12 @@ public class InsertionSort extends SortingAlgorithm {
   }
   
   /**
-   * Run validation tests
-   * @param args command-line args
+   * Run validation test.
+   * @param args command-line arguments
    */
   public static void main(String[] args) {
     SortingAlgorithm.validate(new InsertionSort());
-    System.out.println("Insertion has passed all tests.");
-
-    // Fill an array with random numbers
-    int N = 1433;
-    Integer[] array = new Integer[N];
-    for (int i = 0; i < array.length; i++) {
-      array[i] = (int)(N*Math.random());
-    }
-
-    //Measuring runtime.
-    SortingAlgorithm sorter = new InsertionSort();
-    long start = System.nanoTime();
-    sorter.sort(array);
-    long end = System.nanoTime();
-    double duration = (end-start)/(1e9);
-    System.out.println("Array size: "+N);
-    System.out.println("Total duration: "+duration);
-
-
+    System.out.println("InsertionSort has passed all tests.");
   }
 
 }
